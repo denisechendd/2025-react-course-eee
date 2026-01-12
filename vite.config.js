d1fe691b3141749aa4c06b4dc399f3febdc30549
denisechendd <denisechendd@gmail.com>
2026-01-13 00:09:18 +0800

    update config for changecyyy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/2025-react-course-eee/",
+  base: process.env.NODE_ENV === 'production' ? '/2025-react-course-eee/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
